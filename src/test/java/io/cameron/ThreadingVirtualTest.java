da8edf0effe74ab3462608f8d91909f587770f26
Cameron Gallichan <camerongallichan@gmail.com>
2024-11-25 16:26:43 +0000

    fixes, added todo

--- src/test/java/io/cameron/ThreadingVirtualTest.java
+++ src/test/java/io/cameron/ThreadingVirtualTest.java
@@ -9,22 +9,27 @@ import io.cameron.threading.virtual.Message;
 
 public class ThreadingVirtualTest {
 
+    void sendMessage(Actor actor, Message message) {
+        actor.handle(message);
+    }
+
     @Test
     public void actorTest() throws InterruptedException {
         Actor actor = new Actor();
+        // platform thread
         actor.start();
 
-        actor.handle(new Message(Action.INSERT, 5));
-        actor.handle(new Message(Action.INSERT, 2));
-        actor.handle(new Message(Action.INSERT, 3));
+        // TODO: messages must be handled by virtual threads
+        sendMessage(actor, new Message(Action.INSERT, 5));
+        sendMessage(actor, new Message(Action.INSERT, 2));
+        sendMessage(actor, new Message(Action.INSERT, 3));
 
         assertEquals(true, actor.isAlive());
         assertEquals(List.of(5, 2, 3), actor.getNumbers());
 
-        actor.handle(new Message(Action.EXIT));
+        sendMessage(actor, new Message(Action.EXIT));
 
-        Thread.sleep(100);
-
+        actor.join();
         assertEquals(false, actor.isAlive());
     }
 }
